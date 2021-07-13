d15f00f418be738f5f454a63782d9dbb178e2416
azwidodo <azzahra5widodo@gmail.com>
2021-07-13 14:02:22 +0700

    try selenium

--- selenium_test.py
+++ selenium_test.py
@@ -1,9 +1,23 @@
 from msedge.selenium_tools import Edge, EdgeOptions
-
-# Launch Microsoft Edge (EdgeHTML)
-driver = Edge()
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 # Launch Microsoft Edge (Chromium)
-options = EdgeOptions()
-options.use_chromium = True
-driver = Edge(options=options)
+# PATH = "C:\Program Files (x86)\msedgedriver.exe"
+# options = EdgeOptions()
+# options.use_chromium = True
+# driver = Edge(executable_path=PATH, options=options)
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.goodreads.com/")
+print(driver.title)
+
+search = driver.find_element_by_name("query")
+search.send_keys("9780517884539")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+driver.quit()
